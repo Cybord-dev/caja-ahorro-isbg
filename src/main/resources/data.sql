5dd8306da69b9f63b44d2af41a0c635b68bad8f3
ralfdemoledor <rubens.mncrft@gmail.com>
2020-09-11 15:37:49 -0500

    Corrections

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO USERS(id_user,nombre,nickname,activo) VALUES(1,'Charly1','El tortas',1);
-INSERT INTO USERS(id_user,nombre,nickname,activo) VALUES(2,'Charly2','El tortas',1);
+INSERT INTO USUARIOS(id_usuario,nombre,email,estatus,tipo_usuario) VALUES(1,'Charly1','tortas@.com',1,'INTERNO');
+INSERT INTO USUARIOS(id_usuario,nombre,email,estatus,tipo_usuario) VALUES(2,'Charly2','tortas@.com',1,'INTERNO');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -9,12 +9,12 @@ CREATE TABLE `USER_ROLES` (
   `id_rol` int(11) NOT NULL
 );
 
-CREATE TABLE `USERS` (
-  `id_user` int(11) NOT NULL AUTO_INCREMENT,
-  `user_name` varchar(45) NOT NULL,
-  `email` varchar(55) NOT NULL,
-  `status` tinyint(1) NOT NULL DEFAULT '0',
-  `tipo_user` timestamp,
+CREATE TABLE `USUARIOS` (
+  `id_usuario` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `estatus` tinyint(1) NOT NULL DEFAULT '0',
+  `tipo_usuario` varchar(20),
   `fecha_creacion` timestamp,
   `fecha_actualizacion` timestamp
 );
@@ -22,8 +22,8 @@ CREATE TABLE `USERS` (
 CREATE TABLE `PRESTAMO` (
   `id_prestamo` int(11) NOT NULL AUTO_INCREMENT,
   `id_deudor` int(11) NOT NULL,
-  `status` tinyint(1) NOT NULL DEFAULT '0',
-  `monto` decimal(12,4) NOT NULL,
+  `estatus` tinyint(1) NOT NULL DEFAULT '0',
+  `monto` decimal(10,2) NOT NULL,
   `fecha_terminacion` timestamp NOT NULL,
   `fecha_creacion` timestamp NOT NULL,
   `fecha_actualizacion` timestamp NOT NULL
@@ -31,28 +31,28 @@ CREATE TABLE `PRESTAMO` (
 
 CREATE TABLE `AHORRADOR` (
   `id_ahorrador` int(11) NOT NULL AUTO_INCREMENT,
-  `id_user` int(11) NOT NULL,
-  `monto_ahorro` decimal(12,4) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `monto_ahorro` decimal(10,2) NOT NULL,
   `porcentaje_ahorro` decimal(5,2)  NOT NULL,
-  `antiguedad` varchar(45) NOT NULL,
-  `sueldo` decimal(12,4) NOT NULL,
+  `antiguedad` timestamp NOT NULL,
+  `sueldo` decimal(10,2) NOT NULL,
   `fecha_creacion` timestamp NOT NULL,
   `fecha_actualizacion` timestamp NOT NULL
 );
 
 CREATE TABLE `DEUDOR` (
   `id_deudor` int(11) NOT NULL AUTO_INCREMENT,
-  `id_user` int(11) NOT NULL,
-  `antiguedad` varchar(45) NOT NULL,
-  `sueldo` decimal(12,4) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `antiguedad` timestamp NOT NULL,
+  `sueldo` decimal(10,2) NOT NULL,
   `fecha_creacion` timestamp NOT NULL,
   `fecha_actualizacion` timestamp NOT NULL
 );
 
 CREATE TABLE `DATOS_USER` (
-  `id_user` int(11) NOT NULL,
-  `tipo_dato` varchar(45) NOT NULL,
-  `dato` tinyint(1) NOT NULL DEFAULT '0',
+  `id_datos` int(11) NOT NULL,
+  `tipo_dato` varchar(50) NOT NULL,
+  `dato` varchar(100) NOT NULL DEFAULT '0',
   `relevancia` tinyint(1) NOT NULL DEFAULT '0',
   `fecha_creacion` timestamp NOT NULL,
   `fecha_actualizacion` timestamp NOT NULL
@@ -60,12 +60,12 @@ CREATE TABLE `DATOS_USER` (
 
 CREATE TABLE `SOLICITUDES` (
   `id_solicitud` int(11) NOT NULL AUTO_INCREMENT,
-  `id_user` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
   `tipo_solicitud` tinyint(1) NOT NULL DEFAULT '0',
   `status` tinyint(1) NOT NULL DEFAULT '0',
-  `status_detalle` varchar(45) NOT NULL,
+  `status_detalle` varchar(50) NOT NULL,
   `porcentaje` decimal(5,2)  NOT NULL,
-  `cantidad` decimal(12,4) NOT NULL,
+  `cantidad` decimal(10,2) NOT NULL,
   `fecha_ejecucion` timestamp NOT NULL,
   `fecha_creacion` timestamp NOT NULL,
   `fecha_actualizacion` timestamp NOT NULL
@@ -75,8 +75,8 @@ CREATE TABLE `ATRIBUTOS_SOLICITUD` (
   `id_atributo` int(11) NOT NULL AUTO_INCREMENT,
   `id_solicitud` int(11) NOT NULL,
   `tipo_atributo` tinyint(1) NOT NULL DEFAULT '0',
-  `nombre` tinyint(1) NOT NULL DEFAULT '0',
-  `valor` varchar(45) NOT NULL,
+  `nombre` varchar(50) NOT NULL DEFAULT '0',
+  `valor` varchar(100) NOT NULL,
   `fecha_creacion` timestamp NOT NULL,
   `fecha_actualizacion` timestamp NOT NULL
 );
@@ -85,24 +85,25 @@ CREATE TABLE `VALIDACIONES` (
   `id_validacion` int(11) NOT NULL AUTO_INCREMENT,
   `id_solicitud` int(11) NOT NULL,
   `numero_validacion` tinyint(1) NOT NULL DEFAULT '0',
-  `id_user` int(11) NOT NULL,
-  `status` tinyint(1) NOT NULL DEFAULT '0',
-  `tipo_validacion` tinyint(1) NOT NULL DEFAULT '0',
+  `id_usuario` int(11) NOT NULL,
+  `estatus` tinyint(1) NOT NULL DEFAULT '0',
+  `tipo_validacion` varchar(50) NOT NULL DEFAULT '0',
   `fecha_creacion` timestamp NOT NULL,
   `fecha_actualizacion` timestamp NOT NULL
 );
 
 CREATE TABLE `CAT_PROPIEDADES` (
   `id_atributo` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(45) NOT NULL,
-  `tipo` tinyint(1) NOT NULL DEFAULT '0',
+  `tipo` varchar(50) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `valor` varchar(100) NOT NULL
 );
 
 CREATE TABLE `SALDO_AHORRO` (
   `id_ahorro` int(11) NOT NULL AUTO_INCREMENT,
-  `id_user` int(11) NOT NULL,
-  `tipo` tinyint(1) NOT NULL DEFAULT '0',
-  `monto` decimal(12,4) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `tipo` varchar(50) NOT NULL,
+  `monto` decimal(10,2) NOT NULL,
   `validado` tinyint(1) NOT NULL DEFAULT '0',
   `fecha_creacion` timestamp NOT NULL,
   `fecha_actualizacion` timestamp NOT NULL
@@ -111,18 +112,18 @@ CREATE TABLE `SALDO_AHORRO` (
 CREATE TABLE `SALDO_PRESTAMO` (
   `id_saldo_prestamo` int(11) NOT NULL AUTO_INCREMENT,
   `id_prestamo` int(11) NOT NULL,
-  `tipo` tinyint(1) NOT NULL DEFAULT '0',
-  `monto` decimal(12,4) NOT NULL,
+  `tipo` varchar(50) NOT NULL,
+  `monto` decimal(10,2) NOT NULL,
   `validado` tinyint(1) NOT NULL DEFAULT '0',
   `fecha_creacion` timestamp NOT NULL,
   `fecha_actualizacion` timestamp NOT NULL
 );
 
 CREATE TABLE `RESOURSCES` (
-  `id_resources` int(11) NOT NULL AUTO_INCREMENT,
+  `id_recurso` int(11) NOT NULL AUTO_INCREMENT,
   `referencia` varchar(45) NOT NULL,
   `tipo_referencia` tinyint(1) NOT NULL DEFAULT '0',
-  `documento` varchar(45) NOT NULL,
+  `documento` blob(45) NOT NULL,
   `fecha_creacion` timestamp NOT NULL,
   `fecha_actualizacion` timestamp NOT NULL
 );
