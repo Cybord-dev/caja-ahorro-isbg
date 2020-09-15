1736c52dfe48740f40ac7ff9e69c4968dd9a3cef
Eddieson <editiend@gmail.com>
2020-09-15 11:37:59 -0500

    Update Solicitud.java

--- src/main/java/com/business/cybord/entities/Solicitud.java
+++ src/main/java/com/business/cybord/entities/Solicitud.java
@@ -4,5 +4,12 @@
 package com.business.cybord.entities;
 
 public class Solicitud {
-
+	private int id;
+	private int idUsuario;
+	private String tipo;
+	private String status;
+	private String statusDetalle;
+	
+	
+	
 }
