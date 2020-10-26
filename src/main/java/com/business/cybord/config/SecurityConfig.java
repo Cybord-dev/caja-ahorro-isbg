9a03c4c6851773087255f1e5bc06b9efd75ca9f9
ralfdemoledor <rubens.mncrft@gmail.com>
2020-10-25 19:41:37 -0600

    aditional security config

--- src/main/java/com/business/cybord/config/SecurityConfig.java
+++ src/main/java/com/business/cybord/config/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 //		http.csrf().disable().authorizeRequests().antMatchers("/api/**").permitAll();
 		http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and().authorizeRequests()
-		.anyRequest().authenticated().and().oauth2Login().and().logout().logoutSuccessUrl("/#/login")
+		.anyRequest().authenticated().and().oauth2Login().and().logout().invalidateHttpSession(true).logoutSuccessUrl("/#/login")
 		.invalidateHttpSession(true).deleteCookies("JSESSIONID");
 	}
 }
