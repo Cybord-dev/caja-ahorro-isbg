4bbe2c499e1f610fae3d1a6c2644fc8a30324369
hha0009 <hugo.avelino@walmart.com>
2021-01-26 17:31:31 -0600

    fix merge

--- src/main/java/com/business/cybord/services/PrestamoService.java
+++ src/main/java/com/business/cybord/services/PrestamoService.java
@@ -1,14 +1,16 @@
 package com.business.cybord.services;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
 import com.business.cybord.mappers.PrestamoMapper;
-import com.business.cybord.mappers.SaldoPrestamoMapper;
 import com.business.cybord.models.dtos.PrestamoDto;
 import com.business.cybord.models.entities.Prestamo;
 import com.business.cybord.repositories.PrestamoRepository;
-import java.util.List;
-import java.util.Optional;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
 
 
 
@@ -21,10 +23,7 @@ public class PrestamoService {
 	@Autowired
 	private PrestamoMapper mapper;
 	
-	@Autowired
-	private SaldoPrestamoMapper saldoPrestamoMapper;
 	
-
 
 	public List<PrestamoDto> getPrestamosdeUnUsuarioPorSuId(Integer id) {
 		return mapper.getDtosFromEntity(repository.findByIdDeudor(id));
