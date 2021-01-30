8fa8c8018ca67ebe2385de2a122c800c589323a7
edgar ramos <eddypeacegamer@gmail.com>
2021-01-30 11:28:05 -0600

    fix de mapper

--- src/main/java/com/business/cybord/mappers/PrestamoMapper.java
+++ src/main/java/com/business/cybord/mappers/PrestamoMapper.java
@@ -2,38 +2,35 @@ package com.business.cybord.mappers;
 
 import java.util.List;
 
+import org.mapstruct.DecoratedWith;
 import org.mapstruct.Mapper;
-import org.mapstruct.MapperConfig;
 import org.mapstruct.Mapping;
-import org.mapstruct.ReportingPolicy;
 
 import com.business.cybord.models.dtos.PrestamoDto;
 import com.business.cybord.models.dtos.SaldoPrestamoDto;
 import com.business.cybord.models.entities.Prestamo;
 import com.business.cybord.models.entities.SaldoPrestamo;
 
-
-
 @Mapper
-@MapperConfig(unmappedTargetPolicy = ReportingPolicy.IGNORE)
+@DecoratedWith(PrestamoMapperDecorator.class)
 public interface PrestamoMapper {
-	
-	@Mapping(target="solicitud.atributos",ignore = true)
-	Prestamo getEntityFromDto(PrestamoDto dto);
-	List<Prestamo> getEntitysFromDtos(List<PrestamoDto> dto);
-	
-	@Mapping(target="solicitud.atributos", ignore = true)
-	PrestamoDto getDtoFromEntity(Prestamo dto);
-	List<PrestamoDto> getDtosFromEntity(List<Prestamo> entities);
-	
-	SaldoPrestamo getSaldoEntityFromSaldoDto(SaldoPrestamoDto dto);
-	List<SaldoPrestamo> getSaldoEntitiesFromSaldoDtos(List<SaldoPrestamoDto> dtos);
-	
-	SaldoPrestamoDto getSaldoDtoFromEntity(SaldoPrestamo saldoPrestamo);
-	List<SaldoPrestamoDto> getSaldoDtosFromEntities(List<SaldoPrestamoDto> entities);
-	
-	
-	
 
-	
+	@Mapping(target = "solicitud", ignore = true)
+	Prestamo getEntityFromDto(PrestamoDto dto);
+
+	List<Prestamo> getEntitysFromDtos(List<PrestamoDto> dto);
+
+	@Mapping(target = "solicitud", ignore = true)
+	PrestamoDto getDtoFromEntity(Prestamo dto);
+
+	List<PrestamoDto> getDtosFromEntity(List<Prestamo> entities);
+
+	SaldoPrestamo getSaldoEntityFromSaldoDto(SaldoPrestamoDto dto);
+
+	List<SaldoPrestamo> getSaldoEntitiesFromSaldoDtos(List<SaldoPrestamoDto> dtos);
+
+	SaldoPrestamoDto getSaldoDtoFromEntity(SaldoPrestamo saldoPrestamo);
+
+	List<SaldoPrestamoDto> getSaldoDtosFromEntities(List<SaldoPrestamoDto> entities);
+
 }
