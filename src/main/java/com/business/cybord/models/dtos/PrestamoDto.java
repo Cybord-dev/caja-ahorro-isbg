1dd591bc73dfd7eb648d1ee100f061839cc8811d
ralfdemoledor <rubens.mncrft@gmail.com>
2021-02-14 15:48:02 -0600

    Fixes Prestamos

--- src/main/java/com/business/cybord/models/dtos/PrestamoDto.java
+++ src/main/java/com/business/cybord/models/dtos/PrestamoDto.java
@@ -24,101 +24,85 @@ public class PrestamoDto implements Serializable {
 	private Date fechaCreacion;
 	private Date fechaActualizacion;
 	private List<SaldoPrestamoDto> saldosPrestamo;
-
+	private SolicitudDto solicitud;
 	public int getId() {
 		return id;
 	}
-
 	public void setId(int id) {
 		this.id = id;
 	}
-
-	public String getEstatus() {
-		return estatus;
-	}
-
-	public void setEstatus(String estatus) {
-		this.estatus = estatus;
-	}
-
-	public BigDecimal getMonto() {
-		return monto;
-	}
-
-	public void setMonto(BigDecimal monto) {
-		this.monto = monto;
-	}
-
-	public int getNoQuincenas() {
-		return noQuincenas;
-	}
-
-	public void setNoQuincenas(int noQuincenas) {
-		this.noQuincenas = noQuincenas;
-	}
-
-	public BigDecimal getTasaInteres() {
-		return tasaInteres;
-	}
-
-	public void setTasaInteres(BigDecimal tasaInteres) {
-		this.tasaInteres = tasaInteres;
-	}
-
-	public BigDecimal getSaldoPendiente() {
-		return saldoPendiente;
-	}
-
-	public void setSaldoPendiente(BigDecimal saldoPendiente) {
-		this.saldoPendiente = saldoPendiente;
-	}
-
-	public Date getFechaTerminacion() {
-		return fechaTerminacion;
-	}
-
-	public void setFechaTerminacion(Date fechaTerminacion) {
-		this.fechaTerminacion = fechaTerminacion;
-	}
-
-	public Date getFechaCreacion() {
-		return fechaCreacion;
-	}
-
-	public void setFechaCreacion(Date fechaCreacion) {
-		this.fechaCreacion = fechaCreacion;
-	}
-
-	public Date getFechaActualizacion() {
-		return fechaActualizacion;
-	}
-
-	public void setFechaActualizacion(Date fechaActualizacion) {
-		this.fechaActualizacion = fechaActualizacion;
-	}
-
-	public List<SaldoPrestamoDto> getSaldosPrestamo() {
-		return saldosPrestamo;
-	}
-
-	public void setSaldosPrestamo(List<SaldoPrestamoDto> saldosPrestamo) {
-		this.saldosPrestamo = saldosPrestamo;
-	}
-
 	public int getIdDeudor() {
 		return idDeudor;
 	}
-
 	public void setIdDeudor(int idDeudor) {
 		this.idDeudor = idDeudor;
 	}
-
+	public String getEstatus() {
+		return estatus;
+	}
+	public void setEstatus(String estatus) {
+		this.estatus = estatus;
+	}
+	public BigDecimal getMonto() {
+		return monto;
+	}
+	public void setMonto(BigDecimal monto) {
+		this.monto = monto;
+	}
+	public int getNoQuincenas() {
+		return noQuincenas;
+	}
+	public void setNoQuincenas(int noQuincenas) {
+		this.noQuincenas = noQuincenas;
+	}
+	public BigDecimal getTasaInteres() {
+		return tasaInteres;
+	}
+	public void setTasaInteres(BigDecimal tasaInteres) {
+		this.tasaInteres = tasaInteres;
+	}
+	public BigDecimal getSaldoPendiente() {
+		return saldoPendiente;
+	}
+	public void setSaldoPendiente(BigDecimal saldoPendiente) {
+		this.saldoPendiente = saldoPendiente;
+	}
+	public Date getFechaTerminacion() {
+		return fechaTerminacion;
+	}
+	public void setFechaTerminacion(Date fechaTerminacion) {
+		this.fechaTerminacion = fechaTerminacion;
+	}
+	public Date getFechaCreacion() {
+		return fechaCreacion;
+	}
+	public void setFechaCreacion(Date fechaCreacion) {
+		this.fechaCreacion = fechaCreacion;
+	}
+	public Date getFechaActualizacion() {
+		return fechaActualizacion;
+	}
+	public void setFechaActualizacion(Date fechaActualizacion) {
+		this.fechaActualizacion = fechaActualizacion;
+	}
+	public List<SaldoPrestamoDto> getSaldosPrestamo() {
+		return saldosPrestamo;
+	}
+	public void setSaldosPrestamo(List<SaldoPrestamoDto> saldosPrestamo) {
+		this.saldosPrestamo = saldosPrestamo;
+	}
+	public SolicitudDto getSolicitud() {
+		return solicitud;
+	}
+	public void setSolicitud(SolicitudDto solicitud) {
+		this.solicitud = solicitud;
+	}
 	@Override
 	public String toString() {
 		return "PrestamoDto [id=" + id + ", idDeudor=" + idDeudor + ", estatus=" + estatus + ", monto=" + monto
 				+ ", noQuincenas=" + noQuincenas + ", tasaInteres=" + tasaInteres + ", saldoPendiente=" + saldoPendiente
 				+ ", fechaTerminacion=" + fechaTerminacion + ", fechaCreacion=" + fechaCreacion
-				+ ", fechaActualizacion=" + fechaActualizacion + ", saldosPrestamo=" + saldosPrestamo + "]";
+				+ ", fechaActualizacion=" + fechaActualizacion + ", saldosPrestamo=" + saldosPrestamo + ", solicitud="
+				+ solicitud + "]";
 	}
-	
 }
--- src/main/java/com/business/cybord/models/entities/Prestamo.java
+++ src/main/java/com/business/cybord/models/entities/Prestamo.java
@@ -13,6 +13,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -65,7 +66,10 @@ public class Prestamo {
 	@OneToMany(cascade = CascadeType.ALL)
 	@JoinColumn(name = "id_prestamo", referencedColumnName = "id_prestamo", insertable = false, updatable = false)
 	private List<SaldoPrestamo> saldosPrestamo;
-
+	
+	@OneToOne
+	@JoinColumn(name = "id_solicitud", referencedColumnName = "id_solicitud", updatable = false)
+	private Solicitud solicitud;
 
 	public int getId() {
 		return id;
@@ -155,12 +159,20 @@ public class Prestamo {
 		this.saldosPrestamo = saldosPrestamo;
 	}
 
+	public Solicitud getSolicitud() {
+		return solicitud;
+	}
+
+	public void setSolicitud(Solicitud solicitud) {
+		this.solicitud = solicitud;
+	}
+
 	@Override
 	public String toString() {
 		return "Prestamo [id=" + id + ", idDeudor=" + idDeudor + ", estatus=" + estatus + ", monto=" + monto
 				+ ", noQuincenas=" + noQuincenas + ", tasaInteres=" + tasaInteres + ", saldoPendiente=" + saldoPendiente
 				+ ", fechaTerminacion=" + fechaTerminacion + ", fechaCreacion=" + fechaCreacion
-				+ ", fechaActualizacion=" + fechaActualizacion + ", saldosPrestamo=" + saldosPrestamo + "]";
+				+ ", fechaActualizacion=" + fechaActualizacion + ", saldosPrestamo=" + saldosPrestamo +"]";
 	}
 
 }
