1ea5ba315a557d095d75bf8ef0d70924767c3fba
ralfdemoledor <rubens.mncrft@gmail.com>
2021-02-28 18:28:15 -0600

    Row mapper fix

--- src/main/java/com/business/cybord/utils/extractor/reportes/ReporteAhorroRowMapper.java
+++ src/main/java/com/business/cybord/utils/extractor/reportes/ReporteAhorroRowMapper.java
@@ -1,5 +1,6 @@
 package com.business.cybord.utils.extractor.reportes;
 
+import java.math.BigDecimal;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -12,14 +13,14 @@ public class ReporteAhorroRowMapper  implements RowMapper<ReporteAhorroDto> {
 	@Override
 	public ReporteAhorroDto mapRow(ResultSet rs, int rowNum) throws SQLException {
 		ReporteAhorroDto result = new ReporteAhorroDto();
-		result.setAhorro(rs.getBigDecimal("ahorro"));
-		result.setAjuste(rs.getBigDecimal("ajuste"));
-		result.setInteres(rs.getBigDecimal("interes"));
+		result.setAhorro(rs.getBigDecimal("ahorro")!= null ? rs.getBigDecimal("ahorro"): BigDecimal.ZERO);
+		result.setAjuste(rs.getBigDecimal("ajuste")!= null ? rs.getBigDecimal("ajuste"): BigDecimal.ZERO);
+		result.setInteres(rs.getBigDecimal("interes")!=null ? rs.getBigDecimal("interes"): BigDecimal.ZERO);
 		result.setIdUsuario(rs.getInt("id_usuario"));
 		result.setTipoUsuario(rs.getString("tipo_usuario"));
 		result.setNoEmpleado(rs.getString("no_empleado"));
 		result.setNombre(rs.getString("nombre"));
-		result.setTotal(rs.getBigDecimal("total"));
+		result.setTotal(rs.getBigDecimal("total")!= null ? rs.getBigDecimal("total"): BigDecimal.ZERO);
 		return result;
 	}
 }
